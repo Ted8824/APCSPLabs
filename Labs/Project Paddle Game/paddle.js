6210f2f9d5250d43fd42ac97a035202062cdd6cd
ted <794123@seq.org>
2018-10-04 08:49:59 -0700

    Add the paddle project initial

---
+++ Labs/Project Paddle Game/paddle.js
@@ -0,0 +1,39 @@
+/*
+**  Paddle Constructor Function
+** Ted Ikehara
+** Aug 21, 2018
+*/
+
+function Paddle(location, size, col){
+  // Instance variables
+  this.loc = location;
+  this.size = size;
+  this.col = col;
+
+  // This function calls other functions
+  this.run = function(){
+    this.checkCollision();
+    this.update();
+    this.render();
+  }
+
+  // This function changes the location of the paddle
+  // only
+  this.update = function(){
+
+      var mouseLoc = createVector(mouseX);
+      this.loc = p5.Vector.lerp(this.loc, mouseLoc, .09);
+
+  }
+
+  // This function checks the collision of the numBalls
+  this.checkCollision = function(){
+
+  }
+
+  // render() draws the ball at the new location
+  this.render = function(){
+    fill(this.col);
+    rect(this.loc.x, this.loc.y, this.size, this.size);
+  }
+}
---
+++ Labs/Project Paddle Game/sketch.js
@@ -0,0 +1,37 @@
+//Global variables
+var balls = [];
+var paddle;
+// setup code
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  loadBalls(20);
+}
+
+//This is ran with 30FPS
+function draw() {
+  background(20, 20, 20, 25);
+  Paddle.run();
+  for (i = 0; i < balls.length; i++){
+    balls[i].run();
+  }
+}
+
+// This function loads the balls and the paddle
+function loadBalls(numBalls){
+
+  // This is loading balls
+  for (i = 0; i < numBalls; i++){
+    var location = createVector(random(width), random(height));
+    var velocity = createVector(random(-3, 3), random(-3, 3));
+    var r = random(10, 20);
+    var col = color(random(255), random(255), random(255));
+    balls.push(new Ball(location, velocity, r, col));
+  }
+
+  // This loads the paddle
+  var r = 55;
+  var col = color(255,0,0);
+  paddle = new Paddle(location, 40, col);
+}
