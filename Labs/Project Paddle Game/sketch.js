83ef005748c00790afd7b64e57c461ba18a33bc7
794123 <794123@seq.org>
2018-10-21 16:57:13 -0700

    pretty much done

--- Labs/Project Paddle Game/sketch.js
+++ Labs/Project Paddle Game/sketch.js
@@ -3,8 +3,8 @@ var balls = [];
 var paddle;
 var score = 0;
 var count = 0;
-
 var count2 = 0;
+var count3 = 0
 // load image
 var img;
 var img2;
@@ -17,24 +17,32 @@ var button;
 var startSound;
 var loseSound;
 var winSound;
+var powerUp;
 //time
 var s;
+// paddle size
+var big = 20;
 
-// setup code
-function setup() {
-  var cnv = createCanvas(900, 900);
-  cnv.position((windowWidth-width)/2, 30);
-  background(20, 20, 20);
+//loads images and sounds that are needed
+function preload(){
+  //load the sounds
+  startSound = loadSound('theme.mp3');
+  winSound = loadSound('win.mp3');
+  loseSound = loadSound('die.mp3');
+  powerUp = loadSound('power.mp3');
 
+  //load the images
   img = loadImage("box.png");
   img2 = loadImage("background.jpg");
   img3 = loadImage("marioBoard.jpg");
   img4 = loadImage("over.jpg");
   img5 = loadImage("win.jpg");
+}
 
-  //button = createButton('Play');
-  //button.position(350, 750);
-  //button.mousePressed(loadBalls(2));
+// setup code
+function setup() {
+  var cnv = createCanvas(900, 900);
+  cnv.position((windowWidth-width)/2, 30);
 
   //load initial balls
   loadBalls(2);
@@ -43,38 +51,38 @@ function setup() {
   frameRate(2000);
 
   //plays the start sound
+  startSound.stop();
   startSound.setVolume(1.0);
   startSound.play();
-
-  //shows the time
-  //s = second();
 }
 
-function preload(){
-  //load the sounds
-  soundFormats('mp3', 'ogg');
-  startSound = loadSound('theme.mp3');
-  winSound = loadSound('win.mp3');
-  loseSound = loadSound('die.mp3');
-}
-
-//This is ran with 30FPS
+//This is ran with 2000 FPS
 function draw() {
 
-  fill(0, 255, 0);
-  textSize(50);
-  text('Time: ' + s, 30, 50);
-
-  //runs paddle and balls
+  //load the image
   image(img2, 0 , 0);
-  paddle.run();
-  for (i = 0; i < balls.length; i++){
-    balls[i].run();
-  }
 
   fill(0, 255, 0);
   textSize(20);
   text("Score: " + score, 30, 30);
+
+  //play power up sound every ten Balls
+  if(score === 10 && count3 === 0){
+    powerUp.play();
+    count3++;
+    big = big + 10;
+  }
+  if(score === 20 && count3 === 1){
+    powerUp.play();
+    count3++;
+    big = big + 20;
+  }
+
+  //runs paddle and balls
+  paddle.run();
+  for (i = 0; i < balls.length; i++){
+    balls[i].run();
+  }
 
   //acounts for missing balls if any in the code
   for(var i = 0; i < balls.length; i++){
@@ -86,7 +94,7 @@ function draw() {
   }
 
   //win condition
-  if(score === 30){
+  if(score === 50){
       //this is the text
       image(img5, 0, 0, img5.width * 2.5, img5.height * 4);
       //this stops the sound and play the win sound
@@ -111,7 +119,12 @@ function draw() {
   //Instructions for the game
   fill(255, 140, 0);
   textSize(20);
-  text("Instructions: collect 30 balls to win\nhit bottom of paddle to gain more balls\nhit top of paddle to collect\nif all balls are gone you lose", 450, 30);
+  text("Instructions: collect 50 boxes to win\nhit bottom of paddle to gain more boxes\nhit top of paddle to collect\nif all boxes are gone you lose", 500, 30);
+
+
+  fill(255, 140, 0);
+  textSize(20);
+  text("collect boxes to gain upgrade\nhit bottom of paddle to use upgrade", 175, 30);
 
 }
 
@@ -130,5 +143,5 @@ function loadBalls(numBalls){
   var r = 55;
   var col = color(255,0,0);
   var mouseLoc = createVector(mouseX, 650);
-  paddle = new Paddle(mouseLoc, 20, col);
+  paddle = new Paddle(mouseLoc, big, col);
 }
