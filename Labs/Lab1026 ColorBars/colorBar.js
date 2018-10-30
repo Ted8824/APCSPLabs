4f069e7df22875b7a51e5e1c55e84d1bdd005b54
ted <794123@seq.org>
2018-10-30 08:49:15 -0700

    bubble sort color bar

--- Labs/Lab1026 ColorBars/colorBar.js
+++ Labs/Lab1026 ColorBars/colorBar.js
@@ -1,6 +1,18 @@
 //Color bar
 //Ted Ikehara
 
-function colorBar(size, loc, color){
+function colorBar(w, size, loc, color){
+  this.loc = loc;
+  this.size = size;
+  this.color = color
+  this.w = w;
 
+  this.run = function(){
+    this.render();
+  }
+
+  this.render = function(){
+    fill(color);
+    rect(this.loc.x, this.loc.y, w, size);
+  }
 }
--- Labs/Lab1026 ColorBars/sketch.js
+++ Labs/Lab1026 ColorBars/sketch.js
@@ -4,27 +4,56 @@
 
 //Global variables
 var bars = [];
-
+var bars1 = [];
 // setup code
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(20, 20, 20);
-  loadBars(50);
+  background(0);
+  loadBars(255);
+  bars = bars1;
+  bubbleSort();
+
 }
 
 //draw
 function draw() {
   // Runs the boids and the ball
-  background(20, 20, 20, 30);
-
   for (i = 0; i < bars.length; i++){
     bars[i].run();
-
   }
 }
 
 //loads the bars
-function loadBars(){
+function loadBars(num){
+  for(var i = 0; i < num; i++){
+    var h = 100;
+    var w = 800/num;
+    var loc = createVector((w * i), 100);
+    var col = color(random(255), random(255), random(255));
+    bars.push(new colorBar(w, h, loc, col));
+  }
+}
+
+//bubble sort function
+function bubbleSort(){
+  var temp = 0;
+  for(var i = bars.length - 1; i > 0; i--){
+    for(var j = 0; j < i; j++){
+      if(bars[j] > bars[j + 1]){
+        //swap text
+        temp = bars[j];
+        bars[j] = bars[j + 1];
+        bars[j + 1] = temp;
+      }
+    }
+
+  }
+  for(var i = 0; i < bars.length; i++){
+    bars1[i].loc.y + 100;
+  }
+}
+//select Sortin
+function selectSort(){
 
 }
